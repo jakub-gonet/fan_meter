ea2316e071e0db986f61c1123ce5f489cd36433b
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-13 11:08:38 +0200

    Add pins config in Pins class

---
+++ src/pins.cpp
@@ -0,0 +1,10 @@
+#include <avr/io.h>
+#include <stdint.h>
+
+#include "pins.hpp"
+#include "pins_defines.hpp"
+
+Pins::Pins() {
+  PWM_OUT_DDR |= (1 << PWM_OUT_POS) | (1 << PWM_OUT_NEG);
+  PWM_OUT_PORT &= ~((1 << PWM_OUT_POS) | (1 << PWM_OUT_NEG));
+}

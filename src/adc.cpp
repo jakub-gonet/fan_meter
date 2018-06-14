179211e72cbedf8167151f961da6a6d2e9337ce7
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-14 23:15:36 +0200

    Change ADC operation to free running mode

--- src/adc.cpp
+++ src/adc.cpp
@@ -5,6 +5,7 @@
 Adc::Adc() {
   ADMUX = (1 << REFS0);    // AVcc voltage ref, MUX 000 -> ADC0 as input
   ADCSRA = (1 << ADEN) |   // ADC enabled
-           (1 << ADPS0) |  // prescaler /8 (50-200kHz for max resolution)
-           (1 << ADPS1);
+           (1 << ADPS0) |  // prescaler /8 -> 1MHz - 125kHz
+           (1 << ADPS1) |  // (50-200kHz for max resolution)
+           (1 << ADFR);    // free running mode
 }

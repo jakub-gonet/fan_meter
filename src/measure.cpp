92d75c63a89e95e72b4aebf586f38929a77b3ac4
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-14 22:45:22 +0200

    Add casting to floats in PWM math
    It's kinda important :>

--- src/measure.cpp
+++ src/measure.cpp
@@ -16,17 +16,22 @@ void Measure::update_output(uint16_t measured_voltage, Pwm& pwm) {
   * │   ⇑                 When x is below l1 PWM should be in range <l0, l1), growing towards l0.
   * ┼ h0                  Therefore:
   * ┼ l1
-  * │   ⇓                 f(x) → l1 - (x / (l1 - l0)) * l1,    when x in <h0, top)
+  * │   ⇓                 f(x) →  (l1 - x) / (l1 - l0) * l1,   when x in <h0, top)
   * │   ⇓                 f(x) → ((x - h0) / (h1 - h0)) * h1,  when x in <bottom, l1)
   * ┴ bottom, l0
   */
   if (measured_voltage <= lower_delta) {
     pwm.set_duty_cycle_on_negative_output_pin(
-        lower_delta - (measured_voltage / lower_delta) * pwm.pwm_max_value);
+        (lower_delta - measured_voltage) /
+                      (float)lower_delta 
+        
+        * pwm.pwm_max_value);
+
   } else if (measured_voltage >= higher_delta) {
     pwm.set_duty_cycle_on_positive_output_pin(
         ((measured_voltage - higher_delta) /
-         (pwm.pwm_max_value - higher_delta)) *
-        pwm.pwm_max_value);
+         (float)(pwm.pwm_max_value - higher_delta))
+         
+          * pwm.pwm_max_value);
   }
 }

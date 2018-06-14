e700526dd52a1160760e2bea3ecfd1ad6285ec2f
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-14 23:16:54 +0200

    Remove ADC methods responsible for managing single conversions
    Those aren't needed now, as ADC is in free running mode

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,6 @@ int main() {
   Measure measure(500, 50);
 
   while (true) {
-    if (!adc.is_conversion_in_progress()) {
-      adc.start_one_async_conversion();
-    }
 
     uint16_t measured_voltage = adc.get_last_conversion();
     measure.update_output(measured_voltage, pwm);

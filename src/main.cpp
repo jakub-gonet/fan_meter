aa7041013427336de387079075dfdc54f97d4e33
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-14 13:16:31 +0200

    Add main program loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include "adc.hpp"
+#include "measure.hpp"
 #include "pins.hpp"
 #include "pwm.hpp"
 
@@ -12,6 +13,14 @@ int main() {
   Measure measure(500, 50);
 
   while (true) {
+    if (!adc.is_conversion_in_progress()) {
+      adc.make_one_async_conversion();
+    }
+
+    uint16_t measured_voltage = adc.get_last_conversion();
+    if (measured_voltage != -1) {
+      measure.update_output(measured_voltage, pwm);
+    }
   }
 
   return 0;

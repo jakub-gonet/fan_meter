bebb3e307ccd67d1aa436fb903efa063c47e99a3
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-14 18:33:17 +0200

    Fix formulas calculating PWM value
    Explanation:
    ┬ top, h1             Let x be position on this drawing, x in <bottom, top)
    │   ⇑                 Also, let f be function mapping x to PWM output. When x is above h0 PWM should be in range <h0, h1), growing towards h1.
    │   ⇑	              When x is below l1 PWM should be in range <l0, l1), growing towards l0.
    ┼ h0                  Therefore:
    ┼ l1
    │   ⇓	              f(x) → l1 - (x / (l1 - l0)) * l1,    when x in <h0, top)
    │   ⇓                 f(x) → ((x - h0) / (h1 - h0)) * h1,  when x in <bottom, l1)
    ┴ bottom, l0

--- src/measure.cpp
+++ src/measure.cpp
@@ -5,10 +5,17 @@
 Measure::Measure(const uint16_t threshold, uint8_t hysteresis)
     : threshold(threshold), hysteresis(hysteresis) {}
 
-void Measure::update_output(uint16_t measured_voltage, Pwm pwm) {
-  if (measured_voltage < this->threshold - this->hysteresis) {
-    pwm.set_duty_cycle_on_negative_output_pin(measured_voltage);
-  } else if (measured_voltage > this->threshold + this->hysteresis) {
-    pwm.set_duty_cycle_on_positive_output_pin(measured_voltage);
+void Measure::update_output(uint16_t measured_voltage, const Pwm& pwm) {
+  uint16_t lower_delta = this->threshold - this->hysteresis;
+  uint16_t higher_delta = this->threshold + this->hysteresis;
+
+  if (measured_voltage <= lower_delta) {
+    pwm.set_duty_cycle_on_negative_output_pin(
+        lower_delta - (measured_voltage / lower_delta) * pwm.pwm_max_value);
+  } else if (measured_voltage >= higher_delta) {
+    pwm.set_duty_cycle_on_positive_output_pin(
+        ((measured_voltage - higher_delta) /
+         (pwm.pwm_max_value - higher_delta)) *
+        pwm.pwm_max_value);
   }
 }

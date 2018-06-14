abcaad712adc162a560c962c7c760450eccb85ec
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-15 00:12:32 +0200

    Add method checking if dynamic range mode is enabled

--- src/pins.hpp
+++ src/pins.hpp
@@ -1,6 +1,9 @@
 #ifndef PINS_HPP
 #define PINS_HPP
 
+#include <avr/io.h>
+#include "pins_defines.hpp"
+
 class Pins {
  public:
   /**
@@ -8,6 +11,15 @@ class Pins {
    *
    */
   Pins();
+
+  /**
+   * @brief Check if dynamic mode pin is enabled (connected to ground)
+   *
+   * @return bool
+   */
+  inline bool is_dynamic_range_mode_enabled() {
+    return ~DYNAMIC_MODE_PIN_REG & (1 << DYNAMIC_MODE_PIN);
+  }
 };
 
 #endif  // PINS_HPP

c5fc8b8172bcf6ae5a8cd9b8d513afe17a48b45a
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-15 00:24:55 +0200

    Add updating ADC values in main

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,10 @@ int main() {
 
   while (true) {
     uint16_t measured_voltage = adc.get_last_conversion();
+    bool mode_enabled = pins.is_dynamic_range_mode_enabled();
+    if (mode_enabled) {
+      adc.update_measured_values(measured_voltage);
+    }
     measure.update_output(measured_voltage, pwm);
   }
 }

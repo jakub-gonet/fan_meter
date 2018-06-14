1250b844ea52a9283971b875ef3bfee2aa5b7d35
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-14 20:19:29 +0200

    Inline ADC methods to save some bytes
    Compiler refused to optimize method calls :<

--- src/adc.cpp
+++ src/adc.cpp
@@ -3,19 +3,8 @@
 #include "adc.hpp"
 
 Adc::Adc() {
-  ADMUX |= (1 << REFS0);      // AVcc voltage ref, MUX 000 -> ADC0 as input
-  ADCSRA |= (1 << ADEN)       // ADC enabled
-            | (1 << ADPS0) |  // prescaler /8 (50-200kHz for max resolution)
-            (1 << ADPS1);
-}
-bool Adc::is_conversion_in_progress() {
-  return ADCSRA & (1 << ADSC);
-}
-
-void Adc::start_one_async_conversion() {
-  ADCSRA |= (1 << ADSC);
-}
-
-uint16_t Adc::get_last_conversion() {
-  return ADC;
+  ADMUX = (1 << REFS0);    // AVcc voltage ref, MUX 000 -> ADC0 as input
+  ADCSRA = (1 << ADEN) |   // ADC enabled
+           (1 << ADPS0) |  // prescaler /8 (50-200kHz for max resolution)
+           (1 << ADPS1);
 }

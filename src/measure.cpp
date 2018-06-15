24ea6b2b70a6d3662d27cbd5204aab8c0954444d
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-15 12:07:19 +0200

    Improve update_output method

--- src/measure.cpp
+++ src/measure.cpp
@@ -2,20 +2,6 @@
 #include <avr/io.h>
 #include "pwm.hpp"
 
-  uint16_t lower_delta = this->threshold - this->hysteresis;
-  uint16_t higher_delta = this->threshold + this->hysteresis;
-
-  /*
-  * Explanation:
-  * ┬ top, h1             Let x be position on this drawing, x in <bottom, top)
-  * │   ⇑                 Also, let f be function mapping x to PWM output. When x is above h0 PWM should be in range <h0, h1), growing towards h1.
-  * │   ⇑                 When x is below l1 PWM should be in range <l0, l1), growing towards l0.
-  * ┼ h0                  Therefore:
-  * ┼ l1
-  * │   ⇓                 f(x) →  (l1 - x) / (l1 - l0) * l1,   when x in <h0, top)
-  * │   ⇓                 f(x) → ((x - h0) / (h1 - h0)) * h1,  when x in <bottom, l1)
-  * ┴ bottom, l0
-  */
 Measure::Measure(uint16_t threshold, uint8_t hysteresis, const Pwm& pwm)
     : threshold(threshold),
       hysteresis(hysteresis),
@@ -45,20 +31,31 @@ void Measure::update_measured_values(uint16_t current_value) {
 void Measure::use_dynamic_range(bool enabled) {
   is_dynamic_mode_enabled = enabled;
 }
-
+           
 void Measure::update_output(uint16_t measured_voltage) {
+  uint16_t lower_delta = middle_of_range - hysteresis;
+  uint16_t higher_delta = middle_of_range + hysteresis;
+
+
+  /*
+  * Explanation:
+  *    top ┬ h1            Let `x` be position on this drawing, `x in <bottom, top)`
+  *        │               Also, let f be function mapping `x` to PWM output.
+  *        │               When `x` is above `hight` level, PWM should be in range `<h0, h1>`, growing towards `h1`. 
+  *  hight ┼ h0            When `x` is below `low` level PWM should be in range `<l0, l1>`, growing towards `l0`. 
+  *    low ┼ l1            
+  *        │               Therefore:     
+  *        │                f(x) →  (x - hight) / (top - hight) * (h1 - h0),     when x in <0, top)
+  * bottom ┴ l0             f(x) → (1 - ((x - bot) / (low - bot))) * (l1 - l0),  when x in <bottom, l1)
+  */    
   if (measured_voltage <= lower_delta) {
     pwm.set_duty_cycle_on_negative_output_pin(
-        (lower_delta - measured_voltage) /
-                      (float)lower_delta 
-        
-        * pwm.pwm_max_value);
+        (1 - ((measured_voltage - bottom) / (float)(lower_delta - bottom))) *
+        pwm.pwm_max_value);
 
   } else if (measured_voltage >= higher_delta) {
     pwm.set_duty_cycle_on_positive_output_pin(
-        ((measured_voltage - higher_delta) /
-         (float)(pwm.pwm_max_value - higher_delta))
-         
-          * pwm.pwm_max_value);
+        ((measured_voltage - higher_delta) / (float)(top - higher_delta)) *
+        pwm.pwm_max_value);
   }
 }

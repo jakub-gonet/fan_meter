745f2b81275ec1c48e6d02695ecefee973d9f1b7
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-15 12:10:47 +0200

    Adjust dynamic range logic in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,17 @@ int main() {
   Adc adc;
   Pwm pwm;
 
-  Measure measure(500, 50);
+  Measure measure(500, 50, pwm);
 
   while (true) {
     uint16_t measured_voltage = adc.get_last_conversion();
-    bool mode_enabled = pins.is_dynamic_range_mode_enabled();
-    if (mode_enabled) {
-      adc.update_measured_values(measured_voltage);
+    if (pins.is_dynamic_range_mode_enabled()) {
+      measure.use_dynamic_range(true);
+    } else {
+      measure.use_dynamic_range(false);
     }
-    measure.update_output(measured_voltage, pwm);
+
+    measure.update_measured_values(measured_voltage);
+    measure.update_output(measured_voltage);
   }
 }
